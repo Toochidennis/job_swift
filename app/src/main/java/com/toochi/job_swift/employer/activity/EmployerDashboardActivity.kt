9baaa2849886b8d8afd8d0b0e15748dc2669cc28
Toochidennis <dennistoochukwu@gmail.com>
2023-12-05 10:24:43 +0100

    Update EmployerDashboardActivity

---
+++ app/src/main/java/com/toochi/job_swift/employer/activity/EmployerDashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.toochi.job_swift.employer.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toochi.job_swift.R
+
+class EmployerDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employer_dashboard)
+    }
+}
