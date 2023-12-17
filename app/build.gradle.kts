9034409d43f7298f6fc68d30f7b5de99f85011d0
Toochidennis <dennistoochukwu@gmail.com>
2023-12-17 20:51:42 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("com.squareup.picasso:picasso:2.71828")
