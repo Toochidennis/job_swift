df36d951399579a07080200054f6af992511bf85
Toochidennis <dennistoochukwu@gmail.com>
2023-12-17 20:53:27 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/backend/UserAuth.kt
+++ app/src/main/java/com/toochi/job_swift/backend/UserAuth.kt
@@ -2,6 +2,6 @@ package com.toochi.job_swift.backend
 
 import com.google.firebase.auth.FirebaseAuth
 
-object Auth {
+object UserAuth {
     val instance: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 }
--- app/src/main/java/com/toochi/job_swift/model/User.kt
+++ app/src/main/java/com/toochi/job_swift/model/User.kt
@@ -3,8 +3,8 @@ package com.toochi.job_swift.model
 data class User(
     val email: String,
     val password: String?,
-    val firstName: String,
-    val lastName: String,
+    val firstname: String,
+    val lastname: String,
     val userType: String,
     val profilePhotoUrl: String? =null
 )
