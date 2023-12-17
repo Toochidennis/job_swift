3b426ab9461dd37307380f30111a87bdbea03874
Toochidennis <dennistoochukwu@gmail.com>
2023-12-17 20:52:38 +0100

    Update UserSettingsFragment

--- app/src/main/java/com/toochi/job_swift/user/fragment/UserSettingsFragment.kt
+++ app/src/main/java/com/toochi/job_swift/user/fragment/UserSettingsFragment.kt
@@ -1,4 +1,4 @@
-package com.toochi.job_swift.admin.fragment
+package com.toochi.job_swift.user.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AdminNotificationFragment.newInstance] factory method to
+ * Use the [UserSettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AdminNotificationFragment : Fragment() {
+class UserSettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class AdminNotificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_notification, container, false)
+        return inflater.inflate(R.layout.fragment_user_settings, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class AdminNotificationFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AdminNotificationFragment.
+         * @return A new instance of fragment UserSettingsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AdminNotificationFragment().apply {
+            UserSettingsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
