0ba6c077dbf5acb48b8403f971ee51e34ee6775a
Toochidennis <dennistoochukwu@gmail.com>
2023-12-17 20:49:51 +0100

    Update AddJobBasicsDialogFragment.kt

---
+++ app/src/main/java/com/toochi/job_swift/common/fragments/AddJobBasicsDialogFragment.kt
@@ -0,0 +1,60 @@
+package com.toochi.job_swift.common.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.toochi.job_swift.R
+import com.toochi.job_swift.databinding.FragmentAddJobBasicsDialogBinding
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class AddJobBasicsDialogFragment : DialogFragment() {
+
+    private lateinit var binding:FragmentAddJobBasicsDialogBinding
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog2)
+
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        // Inflate the layout for this fragment
+        binding = FragmentAddJobBasicsDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.continueButton.setOnClickListener {
+            JobDescriptionDialogFragment().show(parentFragmentManager, "job description")
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            AddJobBasicsDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
