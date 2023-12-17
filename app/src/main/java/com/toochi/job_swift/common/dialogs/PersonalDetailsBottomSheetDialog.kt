f200f9f14673c538e11f11666af5475680c81e9a
Toochidennis <dennistoochukwu@gmail.com>
2023-12-17 20:55:03 +0100

    Add

---
+++ app/src/main/java/com/toochi/job_swift/common/dialogs/PersonalDetailsBottomSheetDialog.kt
@@ -0,0 +1,32 @@
+package com.toochi.job_swift.common.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.toochi.job_swift.databinding.FragmentBottomSheetPersonalDetailsBinding
+
+
+class PersonalDetailsBottomSheetDialog : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentBottomSheetPersonalDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBottomSheetPersonalDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
