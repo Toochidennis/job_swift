c6100e84fba69acae5ef32b222a8082387b23336
Toochidennis <dennistoochukwu@gmail.com>
2023-12-21 14:38:29 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/MainActivity.kt
+++ app/src/main/java/com/toochi/job_swift/MainActivity.kt
@@ -2,11 +2,8 @@ package com.toochi.job_swift
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.toochi.job_swift.admin.activity.AdminDashboardActivity
 import com.toochi.job_swift.databinding.ActivityMainBinding
 import com.toochi.job_swift.user.activity.UserDashboardActivity
 
@@ -21,16 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
-
-        binding.signUpButton.setOnClickListener {
-            sharedPreferences.edit().putString("from", "sign up").apply()
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
-
-        binding.singInButton.setOnClickListener {
-            sharedPreferences.edit().putString("from", "sign in").apply()
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
 
         startActivity(Intent(this, UserDashboardActivity::class.java))
         finish()
--- app/src/main/java/com/toochi/job_swift/user/fragment/PasswordDialogFragment.kt
+++ app/src/main/java/com/toochi/job_swift/user/fragment/PasswordDialogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.toochi.job_swift.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,16 +15,18 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PersonalInformationFragment.newInstance] factory method to
+ * Use the [PasswordDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PersonalInformationFragment : Fragment() {
+class PasswordDialogFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -35,7 +38,7 @@ class PersonalInformationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_information, container, false)
+        return inflater.inflate(R.layout.fragment_password_dialog, container, false)
     }
 
     companion object {
@@ -45,12 +48,12 @@ class PersonalInformationFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PersonalInformationFragment.
+         * @return A new instance of fragment PasswordDialogFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PersonalInformationFragment().apply {
+            PasswordDialogFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
