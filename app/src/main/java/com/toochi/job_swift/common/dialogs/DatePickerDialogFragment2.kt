c9dd2648a84c1c525ef6f9d1678d82e6b9610396
Toochidennis <dennistoochukwu@gmail.com>
2023-12-21 14:31:59 +0100

    Add

---
+++ app/src/main/java/com/toochi/job_swift/common/dialogs/DatePickerDialogFragment2.kt
@@ -0,0 +1,19 @@
+package com.toochi.job_swift.common.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.toochi.job_swift.R
+
+class DatePickerDialogFragment2:BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return View.inflate(requireContext(), R.layout.fragment_date_picker_dialog2, container)
+    }
+}
