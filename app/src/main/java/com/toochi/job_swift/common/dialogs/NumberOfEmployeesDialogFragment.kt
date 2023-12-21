b5e247a11b15a61610ebe5ae422658959a9c107a
Toochidennis <dennistoochukwu@gmail.com>
2023-12-21 14:37:18 +0100

    Add

---
+++ app/src/main/java/com/toochi/job_swift/common/dialogs/NumberOfEmployeesDialogFragment.kt
@@ -0,0 +1,36 @@
+package com.toochi.job_swift.common.dialogs
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.toochi.job_swift.R
+import com.toochi.job_swift.databinding.FragmentNumberOfEmployeesDialogBinding
+
+class NumberOfEmployeesDialogFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentNumberOfEmployeesDialogBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentNumberOfEmployeesDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
