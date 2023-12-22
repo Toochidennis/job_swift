845753c950fabcb6e852f65eb6db4f0c8e424013
Toochidennis <dennistoochukwu@gmail.com>
2023-12-22 12:16:16 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/common/dialogs/WorkplaceDialogFragment.kt
+++ app/src/main/java/com/toochi/job_swift/common/dialogs/WorkplaceDialogFragment.kt
@@ -13,11 +13,7 @@ import com.toochi.job_swift.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WorkplaceDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class WorkplaceDialogFragment : BottomSheetDialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
