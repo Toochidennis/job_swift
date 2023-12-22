5196a865ec3f579e20b699722914adc98ebdcdaf
Toochidennis <dennistoochukwu@gmail.com>
2023-12-22 12:18:58 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/common/dialogs/JobTypeDialogFragment.kt
+++ app/src/main/java/com/toochi/job_swift/common/dialogs/JobTypeDialogFragment.kt
@@ -8,8 +8,7 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.toochi.job_swift.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -36,15 +35,7 @@ class JobTypeDialogFragment : BottomSheetDialogFragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JobTypeDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             JobTypeDialogFragment().apply {
