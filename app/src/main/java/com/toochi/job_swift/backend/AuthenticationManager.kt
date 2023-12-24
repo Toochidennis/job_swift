83ebfeb9590303f1aa37f70aeec0353abec1f08d
Toochidennis <dennistoochukwu@gmail.com>
2023-12-24 11:24:41 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/backend/AuthenticationManager.kt
+++ app/src/main/java/com/toochi/job_swift/backend/AuthenticationManager.kt
@@ -1,22 +1,25 @@
 package com.toochi.job_swift.backend
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.net.Uri
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.toochi.job_swift.R
+import com.toochi.job_swift.model.Education
+import com.toochi.job_swift.model.Experience
+import com.toochi.job_swift.model.Job
 import com.toochi.job_swift.model.User
 
 object AuthenticationManager {
     private val auth = UserAuth.instance
 
-    fun registerUser(user: User, onComplete: (Boolean, String?) -> Unit) {
+    fun registerWithEmailAndPassword(user: User, onComplete: (Boolean, String?) -> Unit) {
         auth.createUserWithEmailAndPassword(user.email, user.password!!)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -31,6 +34,7 @@ object AuthenticationManager {
     }
 
     fun registerWithGoogle(
+        context: Context,
         account: GoogleSignInAccount,
         onComplete: (Boolean, String?) -> Unit
     ) {
@@ -41,18 +45,16 @@ object AuthenticationManager {
                 if (task.isSuccessful) {
                     val currentUser = auth.currentUser
                     currentUser?.let {
-                        updateUserProfile(
-                            it,
-                            User(
-                                account.email ?: "",
-                                "",
-                                account.givenName ?: "",
-                                account.familyName ?: "",
-                                "",
-                                account.photoUrl?.toString()
-                            ),
-                            onComplete
-                        )
+                        context.getSharedPreferences("loginDetail", MODE_PRIVATE).edit().apply {
+                            putString("user_id", currentUser.uid)
+                            putString("email", account.email ?: "")
+                            putString("lastname", account.familyName ?: "")
+                            putString("firstname", account.givenName ?: "")
+                            putString("user_type", "user")
+                            putString("photo_url", account.photoUrl?.toString())
+                        }.apply()
+
+                        onComplete(true, null)
                     } ?: run {
                         onComplete(false, task.exception?.message)
                     }
@@ -75,19 +77,6 @@ object AuthenticationManager {
                     onComplete(false, task.exception?.message)
                 }
             }
-
-    }
-
-    fun loginWithGoogle(account: GoogleSignInAccount, onComplete: (Boolean, String?) -> Unit) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    onComplete(true, null)
-                } else {
-                    onComplete(false, task.exception?.message)
-                }
-            }
     }
 
     fun getGoogleSignInClient(context: Context): GoogleSignInClient {
@@ -97,70 +86,6 @@ object AuthenticationManager {
             .build()
 
         return GoogleSignIn.getClient(context, gso)
-    }
-
-    fun updateDisplayName(
-        user: FirebaseUser,
-        newDisplayName: String,
-        onComplete: (Boolean, String?) -> Unit
-    ) {
-        val profileUpdates = UserProfileChangeRequest.Builder()
-            .setDisplayName(newDisplayName)
-            .build()
-
-        user.updateProfile(profileUpdates)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    onComplete(true, null)
-                } else {
-                    onComplete(false, task.exception?.message)
-                }
-            }
-    }
-
-    fun updatePhotoUrl(
-        user: FirebaseUser,
-        newPhotoUrl: String,
-        onComplete: (Boolean, String?) -> Unit
-    ) {
-        val profileUpdates = UserProfileChangeRequest.Builder()
-            .setPhotoUri(Uri.parse(newPhotoUrl))
-            .build()
-
-        user.updateProfile(profileUpdates)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    onComplete(true, null)
-                } else {
-                    onComplete(false, task.exception?.message)
-                }
-            }
-    }
-
-    fun updatePassword(
-        user: FirebaseUser,
-        newPassword: String,
-        oldPassword: String,
-        onComplete: (Boolean, String?) -> Unit
-    ) {
-        // You need to re-authenticate before updating password
-        val credential = EmailAuthProvider.getCredential(user.email!!, oldPassword)
-        user.reauthenticate(credential)
-            .addOnCompleteListener { reauthTask ->
-                if (reauthTask.isSuccessful) {
-                    // Re-authentication successful, now update the password
-                    user.updatePassword(newPassword)
-                        .addOnCompleteListener { updatePasswordTask ->
-                            if (updatePasswordTask.isSuccessful) {
-                                onComplete(true, null)
-                            } else {
-                                onComplete(false, updatePasswordTask.exception?.message)
-                            }
-                        }
-                } else {
-                    onComplete(false, reauthTask.exception?.message)
-                }
-            }
     }
 
 
@@ -196,5 +121,51 @@ object AuthenticationManager {
                 .set(user)
         }
     }
+
+    fun updateUserExperience(
+        userId: String,
+        experience: Experience,
+        onComplete: (Boolean, String?) -> Unit
+    ) {
+        // Implement the logic to update user experience in Firestore
+        // Use FirestoreDB.instance.collection("users").document(userId).collection("experience").document(experienceId).set(experience)
+        TODO("Still coming up")
+    }
+
+    fun updateUserEducation(
+        userId: String,
+        education: Education,
+        onComplete: (Boolean, String?) -> Unit
+    ) {
+        // Implement the logic to update user education in Firestore
+        // Use FirestoreDB.instance.collection("users").document(userId).collection("education").document(educationId).set(education)
+        TODO("Still coming up")
+    }
+
+    // Similar functions for companies, personal details, and other user-related information
+
+    fun postJob(userId: String, job: Job, onComplete: (Boolean, String?) -> Unit) {
+        // Implement the logic to post a job in Firestore
+        // Use FirestoreDB.instance.collection("users").document(userId).collection("jobsPosted").add(job)
+        TODO("Still coming up")
+    }
+
+    fun blockJob(
+        userId: String,
+        jobId: String,
+        reason: String,
+        onComplete: (Boolean, String?) -> Unit
+    ) {
+        // Implement the logic to block a job in Firestore
+        // Use FirestoreDB.instance.collection("users").document(userId).collection("jobsBlocked").document(jobId).set(mapOf("reason" to reason))
+        TODO("Still coming up")
+    }
+
+    fun likeJob(userId: String, jobId: String, onComplete: (Boolean, String?) -> Unit) {
+        // Implement the logic to like a job in Firestore
+        // Use FirestoreDB.instance.collection("users").document(userId).collection("jobsLiked").document(jobId).set(mapOf())
+        TODO("Still coming up")
+    }
+
 
 }
