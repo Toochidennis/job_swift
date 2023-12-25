70b412940f8d15551a75f9e8a5bab3e34c789b87
Toochidennis <dennistoochukwu@gmail.com>
2023-12-25 23:17:38 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/model/Company.kt
+++ app/src/main/java/com/toochi/job_swift/model/Company.kt
@@ -1,11 +1,11 @@
 package com.toochi.job_swift.model
 
 data class Company(
-    val id:String,
-    val title: String,
-    val position: String,
-    val noOfEmployees: String,
-    val description: String,
-    val location: String,
-    val regNumber: String
+    val id: String = "",
+    val title: String = "",
+    val position: String = "",
+    val noOfEmployees: String = "",
+    val description: String = "",
+    val location: String = "",
+    val regNumber: String = ""
 )
