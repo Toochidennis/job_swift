2714fafafecb56dfb4b4d475753fd3e3f1153ed7
Toochidennis <dennistoochukwu@gmail.com>
2023-12-25 23:19:04 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/util/Utils.kt
+++ app/src/main/java/com/toochi/job_swift/util/Utils.kt
@@ -1,16 +1,39 @@
 package com.toochi.job_swift.util
 
-import android.animation.ObjectAnimator
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 object Utils {
 
     fun loadFragment(activity: AppCompatActivity, fragment: Fragment, container: Int) {
         activity.supportFragmentManager.commit {
             replace(container, fragment)
+        }
+    }
+
+    fun currencyFormatter(amount: Double): String {
+        return DecimalFormat("#,###.##").format(amount)
+    }
+
+    fun dateFormatter(date: String, format: String = "default"): String {
+        return try {
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val parseDate = simpleDateFormat.parse(date)!!
+
+            val sdf = when (format) {
+                "default" -> SimpleDateFormat("dd MMMM, yyyy", Locale.getDefault())
+                else -> SimpleDateFormat("dd, MMM", Locale.getDefault())
+            }
+
+            sdf.format(parseDate)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ""
         }
     }
 
