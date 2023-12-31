83f5158012b354007d462576c2e2cf23ab0e9c16
Toochidennis <dennistoochukwu@gmail.com>
2023-12-31 18:36:03 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/model/Experience.kt
+++ app/src/main/java/com/toochi/job_swift/model/Experience.kt
@@ -1,3 +1,12 @@
 package com.toochi.job_swift.model
 
-data class Experience(val userId: String)
+data class Experience(
+    var experienceId: String = "",
+    val jobTitle: String = "",
+    val jobType: String = "",
+    var companyName: String = "",
+    var location: String = "",
+    val workplace: String = "",
+    var startDate: String = "",
+    val endDate: String = ""
+)
