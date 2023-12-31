cca8326f9da4e02265fa457aea509609b8aa6f72
Toochidennis <dennistoochukwu@gmail.com>
2023-12-31 18:34:28 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/util/Constants.kt
+++ app/src/main/java/com/toochi/job_swift/util/Constants.kt
@@ -1,8 +1,23 @@
 package com.toochi.job_swift.util
 
 class Constants {
+
     companion object {
         const val SCOPES = "https://www.googleapis.com/auth/firebase.messaging"
-        const val POST_URL = "https://fcm.googleapis.com/v1/projects/jobswift-d3781/messages:send"
+        const val BASE_URL = "https://fcm.googleapis.com/v1/projects/jobswift-d3781/messages:send"
+        const val CONTENT_TYPE = "application/json"
+        const val CHANNEL_ID = "swift_job"
+        const val CHANNEL_NAME = "swift_job"
+        const val SELECTED = "select"
+        const val DESELECTED = "deselect"
+        const val ADMIN = "admin"
+        const val ADMIN_PASSWORD = "Admin@admin123"
+        const val EMPLOYER = "employer"
+        const val EMPLOYEE = "employee"
+        const val PRESENT = "Present"
+        const val PREF_NAME = "loginDetail"
+        const val USER_ID_KEY = "user_id"
+        const val EMAIL = "email"
+        const val PHONE_NUMBER = "phone number"
     }
 }
