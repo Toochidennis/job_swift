7b40ac4543d3b508b3d0d746461812d77b89ae1e
Toochidennis <dennistoochukwu@gmail.com>
2023-12-31 18:42:42 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/model/User.kt
+++ app/src/main/java/com/toochi/job_swift/model/User.kt
@@ -1,15 +1,25 @@
 package com.toochi.job_swift.model
 
-class User {
-    var email: String = ""
-    var password: String = ""
-    var firstname: String = ""
-    var middleName: String = ""
-    var lastname: String = ""
-    var phoneNumber: String = ""
-    var headline: String = ""
-    var country: String = ""
-    var city: String = ""
-    var userType: String = ""
-    var profilePhotoUrl: String = ""
-}
+import com.google.firebase.firestore.FieldValue
+
+data class User(
+    var profileId: String = "",
+    var email: String = "",
+    var password: String = "",
+    var firstname: String = "",
+    var middleName: String = "",
+    var lastname: String = "",
+    var phoneNumber: String = "",
+    var headline: String = "",
+    var about: String = "",
+    var skillsId: String = "",
+    var skills: String = "",
+    var country: String = "",
+    var address: String = "",
+    var dateOfBirth: String = "",
+    var city: String = "",
+    var userType: String = "",
+    var token: String = "",
+    var profilePhotoUrl: String = "",
+    val regDate: FieldValue = FieldValue.serverTimestamp()
+)
