ab40b96ceb14d9c0c9abfdc49a3951acc62ed773
Toochidennis <dennistoochukwu@gmail.com>
2023-12-31 19:50:50 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/model/Notification.kt
+++ app/src/main/java/com/toochi/job_swift/model/Notification.kt
@@ -1,5 +1,7 @@
 package com.toochi.job_swift.model
 
+import com.google.firebase.firestore.FieldValue
+
 data class Notification(
     val notificationId: String = "",
     val token: String = "",
@@ -7,5 +9,6 @@ data class Notification(
     val body: String = "",
     val userId: String = "",
     val ownerId: String = "",
-    val jobId: String = ""
+    val jobId: String = "",
+    val notificationDate: FieldValue = FieldValue.serverTimestamp()
 )
