a5d26448075b72a67125c1a8bc8d8daf5186750d
Toochidennis <dennistoochukwu@gmail.com>
2024-01-03 17:48:17 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/admin/fragment/AdminHomeFragment.kt
+++ app/src/main/java/com/toochi/job_swift/admin/fragment/AdminHomeFragment.kt
@@ -1,61 +1,190 @@
 package com.toochi.job_swift.admin.fragment
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import com.toochi.job_swift.BR
 import com.toochi.job_swift.R
+import com.toochi.job_swift.backend.AuthenticationManager
+import com.toochi.job_swift.backend.PostJobManager
+import com.toochi.job_swift.common.dialogs.LoadingDialog
+import com.toochi.job_swift.common.dialogs.OnDeleteJobBottomDialog
 import com.toochi.job_swift.databinding.FragmentAdminHomeBinding
-
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.toochi.job_swift.model.PostJob
+import com.toochi.job_swift.user.adapters.GenericAdapter
+import com.toochi.job_swift.util.Constants
+import com.toochi.job_swift.util.Constants.Companion.DELETE_JOB
+import com.toochi.job_swift.util.Constants.Companion.PREF_NAME
+import com.toochi.job_swift.util.Utils
+import java.util.Calendar
 
 
 class AdminHomeFragment : Fragment() {
 
-    private var binding: FragmentAdminHomeBinding? = null
+    private var _binding: FragmentAdminHomeBinding? = null
 
-    private var param1: String? = null
-    private var param2: String? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var jobList = mutableListOf<PostJob>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        binding = FragmentAdminHomeBinding.inflate(inflater, container, false)
-        return binding?.root
+        _binding = FragmentAdminHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initData()
     }
 
-    companion object {
+    private fun initData() {
+        setUpProfile()
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        getAllJobs()
+
+        refreshData()
+    }
+
+    private fun setUpProfile() {
+        with(requireActivity().getSharedPreferences(PREF_NAME, MODE_PRIVATE)) {
+            val firstname = getString("firstname", "")
+            val lastname = getString("lastname", "")
+            val photoUrl = getString("photo_url", "")
+
+            val fullName = "$firstname $lastname"
+
+            binding.userNameTextView.text = fullName
+            binding.greetingsTextView.text = getGreetingMessage()
+
+            if (!photoUrl.isNullOrEmpty()) {
+                Picasso.get().load(photoUrl).into(binding.userImageView)
+            }
+        }
+    }
+
+    private fun getAllJobs() {
+        val loadingDialog = LoadingDialog(requireContext())
+
+        try {
+            loadingDialog.show()
+
+            PostJobManager.getAllPostedJobs { jobs, errorMessage ->
+                if (jobs != null) {
+                    jobList.addAll(jobs)
+
+                    val copiedJobs = jobs.map { job ->
+                        val location =
+                            "${job.location} . ${formatAmount(job.salary, job.salaryRate)}"
+                        val jobType = "${job.jobType} . ${job.workplaceType}"
+
+                        PostJob(
+                            userId = job.userId,
+                            title = job.title,
+                            location = location,
+                            jobType = jobType,
+                            jobEmail = job.calculateDaysAgo()
+                        )
+                    }
+
+                    loadingDialog.dismiss()
+                    binding.errorTextView.isVisible = false
+                    binding.noDataImageView.isVisible = false
+                    setUpAdapter(copiedJobs.toMutableList())
+                } else if (errorMessage == Constants.NOT_AVAILABLE) {
+                    binding.errorTextView.isVisible = true
+                    binding.noDataImageView.isVisible = true
+                } else {
+                    loadingDialog.dismiss()
+                    binding.noDataImageView.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.no_internet
+                        )
+                    )
+                    binding.noDataImageView.isVisible = true
+                    binding.errorTextView.isVisible = false
+                    showToast(errorMessage.toString())
                 }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            loadingDialog.dismiss()
+            showToast("An error occurred.")
+        }
     }
+
+    private fun formatAmount(amount: String, rate: String): String {
+        val formatAmount =
+            "${getString(R.string.naira)} ${Utils.currencyFormatter(amount.toDouble())}"
+        return when (rate) {
+            "Per month" -> "$formatAmount/m"
+            else -> "$formatAmount/yr"
+        }
+    }
+
+    private fun setUpAdapter(postJobList: MutableList<PostJob>) {
+        val jobAdapter = GenericAdapter(
+            postJobList,
+            R.layout.item_user_home,
+            bindItem = { binding, model ->
+                binding.setVariable(BR.job, model)
+                binding.executePendingBindings()
+
+                val saveButton: ImageView = binding.root.findViewById(R.id.saveButton)
+                saveButton.isVisible = model.userId != AuthenticationManager.auth.uid
+
+            }
+        ) { position ->
+            OnDeleteJobBottomDialog(jobList[position])
+                .show(parentFragmentManager, DELETE_JOB)
+        }
+
+        binding.jobRecyclerView.apply {
+            hasFixedSize()
+            adapter = jobAdapter
+        }
+    }
+
+    private fun getGreetingMessage(): String {
+        val calender = Calendar.getInstance()
+        val hourOfDay = calender[Calendar.HOUR_OF_DAY]
+
+        return when {
+            hourOfDay < 12 -> getString(R.string.good_morning)
+            hourOfDay < 18 -> getString(R.string.good_afternoon)
+            else -> getString(R.string.good_evening)
+        }
+    }
+
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun refreshData() {
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            getAllJobs()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        _binding = null
     }
 }
--- app/src/main/java/com/toochi/job_swift/admin/fragment/AdminInsightsFragment.kt
+++ app/src/main/java/com/toochi/job_swift/admin/fragment/AdminInsightsFragment.kt
@@ -5,43 +5,113 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.toochi.job_swift.BR
 import com.toochi.job_swift.R
-
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.toochi.job_swift.backend.PersonalDetailsManager.getAllUsers
+import com.toochi.job_swift.common.dialogs.LoadingDialog
+import com.toochi.job_swift.databinding.FragmentAdminInsightsBinding
+import com.toochi.job_swift.model.User
+import com.toochi.job_swift.user.adapters.GenericAdapter
+import com.toochi.job_swift.util.Constants.Companion.NOT_AVAILABLE
 
 
 class AdminInsightsFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentAdminInsightsBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_insights, container, false)
+        _binding = FragmentAdminInsightsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdminInsightsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar.toolbar)
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+
+        actionBar?.apply {
+            title = "Insights"
+        }
+
+        getUsers()
+
+        refreshData()
+    }
+
+
+    private fun getUsers() {
+        val loadingDialog = LoadingDialog(requireContext())
+        try {
+            getAllUsers { users, exception ->
+                if (users != null) {
+                    users.forEach { user ->
+                        val name = "${user.firstname} ${user.lastname}"
+                        user.firstname = name
+                    }
+
+                    setUpAdapter(users)
+                    loadingDialog.dismiss()
+                } else if (exception == NOT_AVAILABLE) {
+                    loadingDialog.show()
+                    binding.errorTextView.isVisible = true
+                    binding.noDataImageView.isVisible = true
+                } else {
+                    loadingDialog.dismiss()
+                    showToast(exception.toString())
                 }
             }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            loadingDialog.dismiss()
+            showToast(e.message.toString())
+        }
     }
+
+    private fun setUpAdapter(userList: MutableList<User>) {
+        val userAdapter = GenericAdapter(
+            itemList = userList,
+            itemResLayout = R.layout.item_admin_insights,
+            bindItem = { binding, model ->
+                binding.setVariable(BR.user, model)
+                binding.executePendingBindings()
+            }
+        ) {
+
+        }
+
+        binding.employeeRecyclerView.apply {
+            hasFixedSize()
+            adapter = userAdapter
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun refreshData() {
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            getUsers()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
