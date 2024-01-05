50c1f5dc109ed312dca3da3a3c32bd9fa416426c
Toochidennis <dennistoochukwu@gmail.com>
2024-01-05 02:53:06 +0100

    Update

--- app/src/main/java/com/toochi/job_swift/common/activities/WelcomeActivity.kt
+++ app/src/main/java/com/toochi/job_swift/common/activities/WelcomeActivity.kt
@@ -1,12 +1,10 @@
 package com.toochi.job_swift.common.activities
 
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.toochi.job_swift.databinding.ActivityWelcomeBinding
 
-class WelcomeActivity : Activity() {
+class WelcomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWelcomeBinding
 
@@ -14,11 +12,5 @@ class WelcomeActivity : Activity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.continueButton.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }
-
     }
 }
